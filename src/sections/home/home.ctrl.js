5682e3c9bac9faeaf378e0bed4306826dd080335
Rubén Solano <rubensoljim@gmail.com>
2019-10-27 17:22:53 +0100

    main content in home and form validations

--- src/sections/home/home.ctrl.js
+++ src/sections/home/home.ctrl.js
@@ -1,3 +1,13 @@
 myApp.controller('HomeController', ['$scope', '$http', 'myService', '$routeParams', function ($scope, $http, myService, $routeParams) {
+    $scope.actually_page = parseInt($routeParams.page);
+    $scope.rpp = parseInt($routeParams.rpp);
+
+    myService.getPage(5, 1).then(function (data) {
+        $scope.posts = data.data.response;
+    });
+    
+    $scope.getBadges = function(str) {
+        return str.trim().split(" ");
+    }
 
 }]);
